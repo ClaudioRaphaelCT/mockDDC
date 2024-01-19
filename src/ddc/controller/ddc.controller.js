ff21af81729b4e7a7d87d8ba2515fb7ceeffdfef
Claudio Raphael <claudioraphaelct@gmail.com>
2024-01-19 10:15:58 -0300

    ajustando contrato

--- src/ddc/controller/ddc.controller.js
+++ src/ddc/controller/ddc.controller.js
@@ -3,7 +3,7 @@ const { responseOK, responseNOK } = require("../utils/responses");
 exports.postDDC = (req, res) => {
 const {numContrato} = req.body
  
-  if (numContrato == 10) {
+  if (numContrato == 1000000016) {
     res.status(400).json(responseNOK);
   }
   res.status(200).json(responseOK);
